056bd8a0e1f9d0ca135cd9ea46562597166404e5
rehman857 <abdurrehmanawan875@gmail.com>
2026-03-18 16:47:29 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-from modules.clamav_scanner import scan_file
-from modules.logger import log_alert
+from modules.file_monitor import start_monitoring
 
 if __name__ == "__main__":
-    file_path = input("Enter file path to scan: ")
-
-    infected, output = scan_file(file_path)
-
-    if infected:
-        print("[ALERT] Malware detected!")
-        log_alert("file_scan", "HIGH", "Malware detected in " + file_path)
-    else:
-        print("[INFO] File is clean.")
-        log_alert("file_scan", "LOW", "File scanned clean: " + file_path)
-
-    print(output)
+    folder = input("Enter folder path to monitor: ")
+    start_monitoring(folder)
