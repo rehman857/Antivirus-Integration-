f86bf4b3421b5e8b5f4b64306612a2ebfd4ebe43
rehman857 <abdurrehmanawan875@gmail.com>
2026-03-19 06:09:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from modules.file_monitor import start_monitoring
+from modules.process_monitor import monitor_processes
+import threading
 
 if __name__ == "__main__":
     folder = input("Enter folder path to monitor: ")
-    start_monitoring(folder)
+
+    t1 = threading.Thread(target=start_monitoring, args=(folder,))
+    t2 = threading.Thread(target=monitor_processes)
+
+    t1.start()
+    t2.start()
+
+    t1.join()
+    t2.join()
