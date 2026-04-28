62807dd11f793d9c18dcefae4ee6be29285698cc
rehman857 <abdurrehmanawan875@gmail.com>
2026-04-28 16:08:17 +0500

    Refactor log_alert function to use message parameter

--- Modules/logger.py
+++ Modules/logger.py
@@ -3,17 +3,21 @@ from datetime import datetime
 
 LOG_FILE = "logs/alerts.json"
 
-def log_alert(event_type, severity, details):
-    alert = {
+def log_alert(event_type, severity, message):
+    try:
+        with open(LOG_FILE, "r") as f:
+            data = json.load(f)
+    except:
+        data = []
+
+    new_log = {
         "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-        "event_type": event_type,
+        "type": event_type,
         "severity": severity,
-        "details": details
+        "message": message
     }
 
-    try:
-        with open(LOG_FILE, "a") as f:
-            f.write(json.dumps(alert) + "\n")
-    except Exception as e:
-        print("Logging error:", e)
+    data.append(new_log)
 
+    with open(LOG_FILE, "w") as f:
+        json.dump(data, f, indent=4)
